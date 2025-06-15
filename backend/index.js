81570a9a45f9783d4dae8d35736f7922a7256d46
Meet Vora <125785245+MeetVora79@users.noreply.github.com>
2025-06-15 13:02:12 +0530

    established a mongodb connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(cors());
@@ -8,10 +8,16 @@ app.use(express.json());
 
 const port = 4000;
 
+mongoose.connect('mongodb://localhost:27017/BookApp').then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('Error connecting to MongoDB:', err);
+});
+
 app.get('/', (req, res) => {
 	res.send('Hello from the backend!');
 });
 
 app.listen(port, () => {
 	console.log(`Server is running on http://localhost:${port}`);
-});
+}); 
