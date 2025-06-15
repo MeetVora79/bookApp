56fa4a0a318423eee62e265103f2478b79954cc7
Meet Vora <125785245+MeetVora79@users.noreply.github.com>
2025-06-15 16:17:05 +0530

    create addbook page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Home from './Home';
+import AddBooks from './BookActions/addbook';
 import {Route, Routes} from "react-router-dom"
 import NavBar from './navbar';
 
@@ -9,10 +10,11 @@ function App() {
       <NavBar></NavBar>
       <Routes>
         <Route path='/' element={<Home/>} />
+        <Route path='/addbook' element={<AddBooks/>} />
       </Routes>
     </div>
   );
-}
+}  
 
 export default App;
 
---
+++ frontend/src/BookActions/addbook.js
@@ -0,0 +1,153 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+export default function AddBooks() {
+  const [name, setName] = useState("");
+  const [author, setAuthor] = useState("");
+  const [year, setYear] = useState("");
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState("");
+  const navigate = useNavigate()
+  const handleRegister = async (e) => {
+      e.preventDefault();
+      try {
+          const response = await axios.post("http://localhost:4000/books", {
+              name,
+              author,
+              year,
+              description,
+              amount
+          })
+          console.log(response)
+          navigate('/')
+      } catch(error) {
+          console.error("Error creating in:", error.response.data.error)
+      }
+  };
+
+  return (
+    <div className="bg-white py-10 sm:py-10">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:text-center">
+          <form onSubmit={handleRegister}>
+            <div className="space-y-12">
+              <div className="border-b border-gray-900/10 pb-12">
+                <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+                  Add Book
+                </h2>
+                <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                  <div className="sm:col-span-3">
+                    <label
+                      htmlFor="name"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Name
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        autoComplete="given-name"
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                      />
+                    </div>
+                  </div>
+
+                  <div className="sm:col-span-3">
+                    <label
+                      htmlFor="author"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      author
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        type="text"
+                        name="author"
+                        id="author"
+                        autoComplete="family-name"
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        value={author}
+                        onChange={(e) => setAuthor(e.target.value)}
+                      />
+                    </div>
+                  </div>
+
+                  <div className="sm:col-span-3">
+                    <label
+                      htmlFor="year"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      year
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        type="text"
+                        name="year"
+                        id="year"
+                        autoComplete="family-name"
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        value={year}
+                        onChange={(e) => setYear(e.target.value)}
+                      />
+                    </div>
+                  </div>
+
+                  <div className="sm:col-span-3">
+                    <label
+                      htmlFor="ammount"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      ammount
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        type="text"
+                        name="ammount"
+                        id="ammount"
+                        autoComplete="family-name"
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        value={amount}
+                        onChange={(e) => setAmount(e.target.value)}
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="sm:col-span-3">
+                  <label
+                    htmlFor="description"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    description
+                  </label>
+                  <div className="mt-2">
+                    <textarea
+                      name="description"
+                      id="description"
+                      autoComplete="family-name"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      value={description}
+                      onChange={(e) => setDescription(e.target.value)}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="mt-6 flex items-center justify-end gap-x-6">
+              <button
+                type="submit"
+                className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                save
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
